e373906725e386a81430b847531ec7327ba07389
yuta-d <yutaro.d.14@gmail.com>
2021-07-10 22:55:18 +0900

    商品情報のテーブル保存実装

---
+++ app/models/delivaliy_cost.rb
@@ -0,0 +1,9 @@
+class DelivaliyCost < ActiveHash::Base
+  include ActiveHash::Associations
+  has_many :items
+  self.data = [
+    {id: 1, name: '--'},
+    {id: 2, name: '着払い（購入者負担）'},
+    {id: 3, name: '送料込み（購入者負担）'}
+  ]
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,23 @@ class Item < ApplicationRecord
   belongs_to :user
   # has_one :purchase
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :quality
+  belongs_to :delivaliy_cost
+  belongs_to :delivaliy_day
+  belongs_to :prefecture
+
+  with_options presence: true do
+    validates :item_name
+    with_options numericality: { other_than: 1, message: "can't be blank" } do
+      validates :category_id
+      validates :quality_id
+      validates :delivaliy_cost_id
+      validates :delivaliy_day_id
+      validates :prefecture_id
+    end
+    validates :description
+    validates :price
+  end
 end
