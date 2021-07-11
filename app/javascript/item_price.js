55a3592d6083cc4495e93bd8e8160b326251308f
yuta-d <yutaro.d.14@gmail.com>
2021-07-11 10:09:18 +0900

    JSの実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,13 @@
-window.addEventListener('load', () => {
-  console.log("OK");
+window.addEventListener('load', function () {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.round(inputValue * 0.1);
+    console.log(addTaxDom);
+    const profitSale = document.getElementById ("profit");
+    profitSale.innerHTML = (Math.floor(inputValue - addTaxDom.innerHTML));
+    console.log(profitSale);
+  });
 });
