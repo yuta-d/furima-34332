11168da85be1ec15a28a3dea9dee189ed4d9245d
yuta-d <yutaro.d.14@gmail.com>
2021-07-13 11:33:31 +0900

    before_action設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_edit, only: [:edit, :update]
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -19,21 +20,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
-  def edit
-    @item = Item.find(params[:id])
-    redirect_to root_path unless current_user.id == @item.user_id
+  def edit 
   end
 
   def update
-    @item = Item.find(params[:id])
-    if @item.update(item_params)
-       redirect_to root_path
-    else
-       render :edit
-    end
+    @item.update(item_params)
   end
 
 
@@ -44,7 +37,11 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:item_name, :category_id, :quality_id, :delivaliy_cost_id, :delivaliy_day_id, :description, :price, :prefecture_id, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    redirect_to new_user_session_path unless user_signed_in?
+  def set_item
+     @item = Item.find(params[:id])
+  end
+
+  def move_to_edit
+    redirect_to item_path unless current_user.id == @item.user_id
   end
 end
